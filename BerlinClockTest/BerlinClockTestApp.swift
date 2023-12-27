65367e576f26a5d8e08622a9e54d44b17d4c5464
2024-dev2-003 <2024.dev2.003@gmail.com>
2023-12-27 15:13:18 +0100

    init project

---
+++ BerlinClockTest/BerlinClockTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockTestApp.swift
+//  BerlinClockTest
+//
+//  Created by 2024-dev2-003 on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
